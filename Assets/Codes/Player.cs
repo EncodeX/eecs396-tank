a0535446e62a89bdc9e6d37cf4ff9792243114a1
Chris Gustanto <cgustanto@gmail.com>
2017-10-29 19:17:59 -0500

    final changes

--- Assets/Codes/Player.cs
+++ Assets/Codes/Player.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace Codes {
     public class Player : MonoBehaviour {
@@ -6,13 +7,19 @@ namespace Codes {
 
         private Rigidbody2D _rb;
 	    private Gun _gun;
+        public Vector2 velocity;
+        public float speed;
+        public float angle;
 
         // Use this for initialization
         internal void Start() {
             _rb = GetComponent<Rigidbody2D>();
 		    _gun = GetComponent<Gun>();
+            velocity = new Vector2(1,0); // new kinematic velocity for player
 
             _fireaxis = Platform.GetFireAxis(gameObject.name);
+            angle = 180f;
+            speed = 2f;
         }
 
         // Update is called once per frame
@@ -21,11 +28,40 @@ namespace Codes {
         }
 
         private void HandleInput() {
-            Turn(Input.GetAxis("Horizontal"), Input.GetAxis("Horizontal2"));
-            Thrust(Input.GetAxis("Vertical"), Input.GetAxis("Vertical2"));
+            //Turn(Input.GetAxis("Horizontal"), Input.GetAxis("Horizontal2"));
+            //Thrust(Input.GetAxis("Vertical"), Input.GetAxis("Vertical2"));
+            Calc(Input.GetAxis("Horizontal"), Input.GetAxis("Horizontal2"), Input.GetAxis("Vertical"), Input.GetAxis("Vertical2"));
             if (Input.GetAxis(_fireaxis) > 0.8f) {
                 Fire(gameObject.GetComponent<SpriteRenderer>().color);
             }
+        }
+
+        private void Calc(float axisX1, float axisX2, float axisY1, float axisY2)
+        {
+            float axisX = gameObject.name == "Player1" ? axisX1 : axisX2;
+            float axisY = gameObject.name == "Player1" ? axisY1 : axisY2;
+            
+            if (Mathf.Abs(axisX) < 0.2f)
+            {
+                axisX = 0;
+            }
+
+            if (Mathf.Abs(axisY) < 0.2f)
+            {
+                axisY = 0;
+            }
+            
+            Vector2 movement = ((Vector2)transform.up) * axisY * speed * Time.deltaTime;
+
+            _rb.MovePosition(_rb.position + movement);
+            
+            float turn = axisX * angle * Time.deltaTime;
+
+            // Make this into a rotation in the y axis.
+            // Quaternion turnRotation = Quaternion.Euler (0f, turn, 0f);
+
+            // Apply this rotation to the rigidbody's rotation.
+            _rb.MoveRotation (_rb.rotation + turn);
         }
 
         private void Turn(float direction1, float direction2) {
@@ -33,17 +69,33 @@ namespace Codes {
             if (Mathf.Abs(direction) < 0.02f) {
                 return;
             }
-            Vector2 placeh;
-            _rb.AddTorque(direction * -0.05f);
-            //_rb.MovePosition(_rb.position + placeh * Time.fixedDeltaTime);
+            
+            //_rb.AddTorque(direction * -0.05f);  
+            // _rb.MoveRotation(_rb.rotation + direction * Time.fixedDeltaTime);
+            if (direction > 0)
+            {
+                angle += 22.5f;
+            }
+            else
+            {
+                angle -= 22.5f;
+            }
+            
         }
 
         private void Thrust(float intensity1, float intensity2) {
             float intensity = gameObject.name == "Player1" ? intensity1 : intensity2;
-            if (Mathf.Abs(intensity) < 0.02f) {
+            if (Mathf.Abs(intensity) < 0.02f)
+            {
+                speed = 0;
                 return;
             }
-            _rb.AddRelativeForce(Vector2.up * intensity);
+            speed = 1;
+             velocity = new Vector2(intensity, intensity);
+            //_rb.AddRelativeForce(Vector2.up * intensity);
+             // _rb.MovePosition(_rb.position + velocity * Math.);
+
+
         }
 
         private void Fire(Color color) {

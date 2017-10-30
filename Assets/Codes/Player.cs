4374d1c176703950e9eaafd1f9b0af4f6aa4c8df
Chris Gustanto <cgustanto@gmail.com>
2017-10-29 19:20:05 -0500

    input changes

--- Assets/Codes/Player.cs
+++ Assets/Codes/Player.cs
@@ -64,39 +64,7 @@ namespace Codes {
             _rb.MoveRotation (_rb.rotation + turn);
         }
 
-        private void Turn(float direction1, float direction2) {
-            float direction = gameObject.name == "Player1" ? direction1 : direction2;
-            if (Mathf.Abs(direction) < 0.02f) {
-                return;
-            }
-            
-            //_rb.AddTorque(direction * -0.05f);  
-            // _rb.MoveRotation(_rb.rotation + direction * Time.fixedDeltaTime);
-            if (direction > 0)
-            {
-                angle += 22.5f;
-            }
-            else
-            {
-                angle -= 22.5f;
-            }
-            
-        }
 
-        private void Thrust(float intensity1, float intensity2) {
-            float intensity = gameObject.name == "Player1" ? intensity1 : intensity2;
-            if (Mathf.Abs(intensity) < 0.02f)
-            {
-                speed = 0;
-                return;
-            }
-            speed = 1;
-             velocity = new Vector2(intensity, intensity);
-            //_rb.AddRelativeForce(Vector2.up * intensity);
-             // _rb.MovePosition(_rb.position + velocity * Math.);
-
-
-        }
 
         private void Fire(Color color) {
             _gun.Fire(color);
